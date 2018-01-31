649bb2ba278a8ecf3a46d9024380cb424dbef51d
steven1028 <yulin.shie@gmail.com>
2018-01-31 15:40:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def makeWebhookResult(req):
         return {}
     result = req.get("result")
     parameters = result.get("parameters")
-    zone = parameters.get("shipping-zone")
+    zone = parameters.get("shipping-zone")[0]
 
     cost = {'Europe':100, 'North America':200, 'South America':300, 'Asia':400, 'Africa':500}
 
@@ -51,8 +51,8 @@ def makeWebhookResult(req):
 
 
 if __name__ == '__main__':
-    port = int(os.getenv('PORT', 443))
+    port = int(os.getenv('PORT', 8000))
 
-    print "Starting app on port %d" % port
+    print("Starting app on port %d" % port)
 
     app.run(debug=True, port=port, host='0.0.0.0')
